94f719153488cf1919a64786e75ab7b5435905dd
luketucker28 <ltuckerb10@gmail.com>
2015-03-22 19:20:00 -0500

    Testing added

---
+++ CigaretteCessationWebApp/CigaretteCessationWebApp/Repository/ConsumedRepository.cs
@@ -0,0 +1,125 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CigaretteCessationWebApp.Models;
+using System.Data.Entity;
+
+
+namespace CigaretteCessationWebApp.Repository
+{
+
+    public class ConsumedRepository
+    {
+        private CigaretteCessationContext _dbContext;
+
+        public ConsumedRepository()
+        {
+            _dbContext = new CigaretteCessationContext();
+            _dbContext.Consumeds.Load();
+
+        }
+        public CigaretteCessationContext Context()
+        {
+            return _dbContext;
+        }
+
+        public DbSet<Consumed> GetDbSet()
+        {
+            return _dbContext.Consumeds;
+        }
+
+        public int GetCount()
+        {
+            return _dbContext.Consumeds.Count<Consumed>();
+
+        }
+      
+        public void Clear()
+        {
+            var b = this.All();
+            _dbContext.Consumeds.RemoveRange(b);
+        }
+
+        public IEnumerable<Consumed> All()
+        {
+            var qu = from Consumed in _dbContext.Consumeds select Consumed;
+            return qu.ToList<Consumed>();
+        }
+
+        public List<Consumed> GetByUserName(string username)
+        {
+            return _dbContext.Consumeds.Where(s => s.UserName == username).ToList();
+        }
+
+        public List<Consumed> GetByDate(string usesname, DateTime D)
+        {
+            return _dbContext.Consumeds.Where(c=> c.Date == D &&  c.UserName == usesname).ToList<Consumed>();
+        }
+
+        //public List<Consumed> GetByDate(string usesname, DateTime D)
+        //{
+        //    return _dbContext.Consumeds.Where(c => c.Date == D && c.UserName == usesname).ToList<Consumed>();
+        //}
+
+        public List<Consumed> GetByName(string users)
+        {
+            var a = from Consumed in _dbContext.Consumeds
+                    where Consumed.UserName == users
+                    select Consumed;
+
+            return a.ToList<Consumed>();
+
+
+        }
+
+        public IEnumerable<Consumed> OrderByName()
+        {
+            var query = (from Consumed in _dbContext.Consumeds
+                         orderby Consumed.UserName
+                         select Consumed).ToList();
+            return query;
+        }
+        public IEnumerable<Consumed> OrderById()
+        {
+            var query = (from Consumed in _dbContext.Consumeds
+                         orderby Consumed.ConsumedID
+                         select Consumed).ToList();
+            _dbContext.SaveChanges();
+            return query;
+        }
+        public Consumed FirstEntry()
+        {
+
+            var query = from Consumed in _dbContext.Consumeds select Consumed;
+            return query.ToList<Consumed>().ElementAt<Consumed>(0);
+
+        }
+        public Consumed FindEntry(int i)
+        {
+
+            var query = from Consumed in _dbContext.Consumeds select Consumed;
+            return query.ToList<Consumed>().ElementAt<Consumed>(i);
+
+        }
+        public void Delete(Consumed E)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<Consumed> PastConsumeds()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Dispose()
+        {
+
+            _dbContext.Dispose();
+        }
+    }
+}
---
+++ CigaretteCessationWebApp/CigaretteCessationWebApp/Repository/TargetRepository.cs
@@ -0,0 +1,133 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CigaretteCessationWebApp.Models;
+using System.Data.Entity;
+
+
+namespace CigaretteCessationWebApp.Repository
+{
+
+    public class TargetRepository
+    {
+        private CigaretteCessationContext _dbContext;
+
+        public TargetRepository()
+        {
+            _dbContext = new CigaretteCessationContext();
+            _dbContext.Targets.Load();
+
+        }
+        public CigaretteCessationContext Context()
+        {
+            return _dbContext;
+        }
+
+        public DbSet<Target> GetDbSet()
+        {
+            return _dbContext.Targets;
+        }
+
+        public int GetCount()
+        {
+            return _dbContext.Targets.Count<Target>();
+
+        }
+
+        public void Add(Target E)
+        {
+            _dbContext.Targets.Add(E);
+            _dbContext.SaveChanges();
+        }
+
+        public void Clear()
+        {
+            var b = this.All();
+            _dbContext.Targets.RemoveRange(b);
+        }
+
+        public IEnumerable<Target> All()
+        {
+            var qu = from Target in _dbContext.Targets select Target;
+            return qu.ToList<Target>();
+        }
+
+        public Target GetById(int id)
+        {
+            var query = from Target in _dbContext.Targets
+                        where Target.TargetID == id
+                        select Target;
+            return query.First<Target>();
+
+        }
+
+        //public Target GetByDate(DateTime D)
+        //{
+        //    var query = from Target in _dbContext.Targets
+        //                where Target.Date == D
+        //                select Target;
+        //    return query.ToList<Target>();
+        //}
+
+        public List<Target> GetByName(string users)
+        {
+            var a = from Target in _dbContext.Targets
+                    where Target.UserName == users
+                    select Target;
+
+            return a.ToList<Target>();
+
+
+        }
+
+        public IEnumerable<Target> OrderByName()
+        {
+            var query = (from Target in _dbContext.Targets
+                         orderby Target.UserName
+                         select Target).ToList();
+            return query;
+        }
+        public IEnumerable<Target> OrderById()
+        {
+            var query = (from Target in _dbContext.Targets
+                         orderby Target.TargetID
+                         select Target).ToList();
+            _dbContext.SaveChanges();
+            return query;
+        }
+        public Target FirstEntry()
+        {
+
+            var query = from Target in _dbContext.Targets select Target;
+            return query.ToList<Target>().ElementAt<Target>(0);
+
+        }
+        public Target FindEntry(int i)
+        {
+
+            var query = from Target in _dbContext.Targets select Target;
+            return query.ToList<Target>().ElementAt<Target>(i);
+
+        }
+        public void Delete(Target E)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<Target> PastTargets()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Dispose()
+        {
+
+            _dbContext.Dispose();
+        }
+    }
+}
