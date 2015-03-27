5aa63b55f3a655687ff1dec0c181b0510edc1bd4
luketucker28 <ltuckerb10@gmail.com>
2015-03-26 21:23:04 -0500

    DatePicker etc working

--- CigaretteCessationWebApp/CigaretteCessationWebApp/Scripts/Angular/Apps/EnterCigarettesController.js
+++ CigaretteCessationWebApp/CigaretteCessationWebApp/Scripts/Angular/Apps/EnterCigarettesController.js
@@ -1,14 +1,8 @@
-﻿; (function () {
+﻿;(function () {
     'use strict';
     angular.module('Quitting')
-    .controller('EnterCigarettesController', function ( $routeParams, USERID, $location, $scope) {
+    .controller('EnterCigarettesController', function ( $routeParams, $location, $scope) {
         var vm = this;
         console.log("qefewqf");
-        })
-
-       
-    
-
-
-    
+    })    
 })();
--- CigaretteCessationWebApp/CigaretteCessationWebApp/Scripts/Angular/Apps/main.js
+++ CigaretteCessationWebApp/CigaretteCessationWebApp/Scripts/Angular/Apps/main.js
@@ -1,18 +1,19 @@
 ﻿/// <reference path="main.js" />
 
-  var Quitting = angular.module('Quitting', ['ngRoute', 'ngResource'])
+  var Quitting = angular.module('Quitting', ['ngRoute', 'ngResource', 'ui.bootstrap'])
        .config(function ($routeProvider, $locationProvider) {
 
 
            $routeProvider.when('/EnterCigarettes', {
                templateUrl: '/Scripts/Angular/templates/EnterCigarettes.html',
                controller: 'EnterCigarettesController',
- 
+               controllerAs: 'Enter'
 
            })
                         .when('/ViewEntries', {
                         templateUrl: '/Scripts/Angular/templates/ViewEntries.html',
-               controller: 'ViewEntriesController'
+                        controller: 'ViewEntriesController',
+                        controllerAs: 'View'
            });
           
 
